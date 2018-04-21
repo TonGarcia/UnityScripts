a57654ff3cb73ced109e7fed8070a4c849850e29
Ilton Silveira <iltonsilveira91@gmail.com>
2018-04-21 14:47:48 -0300

    moviment & camera follow scripts added

---
+++ src/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Follow smoothed Speed rate (greate = faster)
+	public float smoothSpeed = 0.125f;
+
+	// Object targeted by the camera  
+	// (remember that it created a var on camera inspector, drag&drop the char on it) 
+	public Transform targetCharacter;
+
+	// The camera got the char feet position, so create an offset to fix it
+	// (it will create the offset var on inspector)
+	public Vector3 offset;
+
+	// It might update it position on Target move
+	void LateUpdate() {
+		// Update it camera position with the target position
+		transform.position = targetCharacter.position + offset;
+	}
+
+
+}
---
+++ src/Moviment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moviment : MonoBehaviour {
+
+	private Animator moviment;
+
+	// Use this for initialization
+	void Start () {
+
+		moviment = this.GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Toggle moving animation
+		if(Input.GetKey(KeyCode.W)) moviment.SetBool("moving", true);
+		if(Input.GetKeyUp(KeyCode.W)) moviment.SetBool("moving", false);
+
+		// Toggle run or walking
+		if (Input.GetKeyUp (KeyCode.Slash)) {
+			if (moviment.GetBool("toRun") == true) moviment.SetBool ("toRun", false);
+			else moviment.SetBool ("toRun", true);
+		}
+
+	}
+}
